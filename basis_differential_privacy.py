ee0df15c429e988a4be82e5a8acb302f0b2d96f3
forestneo <dr.forestneo@gmail.com>
2019-05-31 13:46:19 +0800

    add basis dp

---
+++ basis_differential_privacy.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-31 12:48
+# @Author  : ForestNeo
+# @Email   : dr.forestneo@gmail.com
+# @Software: PyCharm
+
+#
+import numpy as np
+
+
+def epsilon2probability(epsilon, n=2):
+    return np.e ** epsilon / (np.e ** epsilon + n - 1)
+
+
+def discretization(value, lower, upper):
+    """discretiza values
+    :param value: value that needs to be discretized
+    :param lower, the lower bound of discretized value
+    :param upper: the upper bound of discretized value
+    :return: the discretized value
+    """
+    if value > upper or value < lower:
+        raise Exception("the range of value is not valid in Function @Func: discretization")
+
+    p = (value - lower) / (upper - lower)
+    rnd = np.random.random()
+    return upper if rnd < p else lower
+
+
+def perturbation(value, perturbed_value, epsilon):
+    """
+    perturbation, (random response is a kind of perturbation)
+    :param value: the original value
+    :param perturbed_value: the perturbed value
+    :param epsilon: privacy budget
+    :return: dp version of perturbation
+    """
+    p = np.e ** epsilon / (1 + np.e ** epsilon)
+    rnd = np.random.random()
+    return value if rnd < p else perturbed_value
+
+
+def random_response(value, epsilon):
+    if value not in [0, 1]:
+        raise Exception("The input value is not in [0, 1] @Func: random_response")
+    return perturbation(value=value, perturbed_value=1-value, epsilon=epsilon)
+
+
+def random_response_adjust(sum, N, epsilon):
+    """
+    对random response的结果进行校正
+    :param sum: 收到数据中1的个数
+    :param N: 总的数据个数
+    :return: 实际中1的个数
+    """
+    p = epsilon2probability(epsilon)
+    return (sum + p*N - N) / (2*p - 1)
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
---
+++ mean_estimation.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-31 13:31
+# @Author  : ForestNeo
+# @Email   : dr.forestneo@gmail.com
+# @Software: PyCharm
+
+#
+
+import numpy as np
+import basis_differential_privacy as dp
+
+
+def mean_estimation_experiment():
+    data = np.clip(np.random.normal(loc=0.5, scale=0.2, size=[100000]), 0, 1)
+    print(data)
+
+    mean = np.average(data)
+    print("the mean of original data is: ", mean)
+
+    epsilon = 0.5
+
+    discredized_data = [dp.discretization(value=value, lower=0, upper=1) for value in data]
+    dp_data = [dp.random_response(value=value, epsilon=epsilon) for value in discredized_data]
+
+    cnt_one = np.sum(dp_data)
+    est_one = dp.random_response_adjust(sum=cnt_one, N=len(dp_data), epsilon=epsilon)
+    est_mean = est_one / len(dp_data)
+
+    print("the estimated mean is: ", est_mean)
+
+if __name__ == '__main__':
+    mean_estimation_experiment()
+
+
+
+

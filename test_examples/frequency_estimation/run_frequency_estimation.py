b35000fbe8e7f2bd540efebbfb6c8ee538afeccc
ForestNeo <dr.forestneo@gmail.com>
2021-08-08 15:26:50 +0800

    'test'

--- test_examples/frequency_estimation/run_frequency_estimation.py
+++ test_examples/frequency_estimation/run_frequency_estimation.py
@@ -16,6 +16,7 @@ def run(data):
     this is a frequency estimation example
     """
     frequency = np.sum(data) / len(data)
+    print("data volume is ", len(data))
     print("baseline frequency = ", frequency)
 
     epsilon = 1
@@ -25,11 +26,6 @@ def run(data):
     estimated_frequency = (f + p - 1) / (2 * p - 1)
     print("estimated frequency = ", estimated_frequency)
     print("estimation error = ", np.fabs(estimated_frequency-frequency))
-
-
-def generate_data():
-    data = np.random.binomial(n=1, p=0.8, size=100000)
-    return data
 
 
 if __name__ == '__main__':
